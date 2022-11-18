5516044d0253a057f1b617ac93d8ff02b27de1f5
mohamedmagdy20 <mohamedmagdymohamed982@gmail.com>
2022-11-19 01:35:54 +0200

    cart

--- app/Http/Controllers/api/CartController.php
+++ app/Http/Controllers/api/CartController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Cart;
+use Illuminate\Support\Facades\Validator;
 
 class CartController extends Controller
 {
@@ -33,4 +34,85 @@ class CartController extends Controller
  
         }
     }
+    public function create(Request $request)
+    {
+        $rules = [
+            'product_id'=>'required',
+            'stock'=>'required|numeric'
+        ];
+        $validate = Validator::make($request->all(),$rules);
+        if($validate->fails())
+        {return response()->json(['error'=>$validate->errors()]);}
+
+        if(Cart::create(array_merge($validate->validated(),['client_id'=>auth()->user()->id])))
+        {
+            $product =  Product::where('id',$request->product_id)->with('images')->get();
+            return response()->json([
+                'msg'=>'product added to cart',
+                'data'=>$product,
+                'state'=>true
+            ], 200);
+        }else{
+            return response()->json([
+                'msg'=>'error Occure',
+                'state'=>false
+            ], 400);
+        }
+
+    }
+
+    public function update(Request $request)
+    {
+        $rules = [
+            'id'=>'required',
+            'product_id'=>'required',
+            'stock'=>'required|numeric'
+        ];
+        $validate = Validator::make($request->all(),$rules);
+        if($validate->fails())
+        {return response()->json(['error'=>$validate->errors()]);}
+        $product = Cart::find($request->id);
+        if($product)
+        {
+         $product =  $product->update(array_merge($validate->validated(),[
+            'client_id'=>auth()->user()->id,
+         ]));
+         return response()->json([
+            'msg'=>'cart Updated',
+            'state'=>ture,
+            'data'=>$product
+        ], 200);
+        }else{
+            return response()->json([
+                'msg'=>'error Occure',
+                'state'=>false
+            ], 400);
+        }
+    }
+    public function delete($id)
+    { 
+        $cart = Cart::find($id);
+        if($cart)
+        {
+            $isdeleted =  $cart->delete();
+            if($isdeleted)
+            {
+                return response()->json([
+                    'msg'=>'product removed',
+                    'state'=>true
+                ], 200); 
+            }else{
+                return response()->json([
+                    'msg'=>'error Occure',
+                    'state'=>false
+                ], 400);
+            }
+        }else{
+            return response()->json([
+                'msg'=>'data not found',
+                'state'=>false
+            ], 400);
+        }
+    }
+
 }
