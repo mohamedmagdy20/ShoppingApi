7d7db4baef9e88e722585c04f0f00d90d2d0a19a
mohamedmagdy20 <mohamedmagdymohamed982@gmail.com>
2022-11-20 18:13:07 +0200

    orders

--- app/Http/Controllers/api/OrderController.php
+++ app/Http/Controllers/api/OrderController.php
@@ -48,4 +48,52 @@ class OrderController extends Controller
             ], 400);
         }
     }
+
+    public function createOrderDetails($order_id,$carts)
+    {
+        foreach($carts as $cart)
+        {
+            $orderDetails = new orderDetails();
+            $orderDetails->order_id =  $order_id;
+            $orderDetails->product_id = $cart->product_id;
+            $orderDetails->stock = $cart->stock;
+            $orderDetails->save();
+        }
+        return true;
+    }
+    public function create(Request $request)
+    {
+        //get products from cart 
+        $carts = Cart::where('client_id',auth()->user()->id)->get();
+        if($cart)
+        {
+            $order = new Orders();
+            $order->client_id = auth()->user()->id;
+            $order->save();  
+            $orderDetails  = $this->createOrderDetails($order->id,$carts);
+            if($orderDetails)
+            {
+                $carts = $carts->delete();
+                return response()->json([
+                    'msg'=>'Order Submited',
+                    'data'=>$order->with('orderDetails')->get(),
+                    'state'=>true
+                ], 200);
+            }
+            else
+            {
+                return response()->json([
+                    'msg'=>'Error Occure',
+                    'state'=>false
+                ], 400);
+            }
+
+        }
+        else{
+            return response()->json([
+                'msg'=>'data no found',
+                'state'=>false
+            ], 400);
+        }
+    }
 }
